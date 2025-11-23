8c665da02ecfe372920dd2484b4e997e15fac734
orrya <admin@orrya.co.uk>
2025-11-23 21:31:00 +0000

    Delete app/lib directory

--- app/lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-npm install @supabase/supabase-js
-
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
